48475d7b260c6fce3c1def4805f618f7940c5f8d
author of sylllys <>
2021-09-05 15:16:33 +1000

    added suite config class

---
+++ src/main/java/io/github/sylllys/cucumber/utilities/SuiteConfigDetails.java
@@ -0,0 +1,24 @@
+package io.github.sylllys.cucumber.utilities;
+
+public class SuiteConfigDetails {
+
+    public String getDir() {
+        return dir;
+    }
+
+    public void setDir(String dir) {
+        this.dir = dir;
+    }
+
+    public String getEnv() {
+        return env;
+    }
+
+    public void setEnv(String env) {
+        this.env = env;
+    }
+
+    private String dir;
+    private String env;
+
+}
